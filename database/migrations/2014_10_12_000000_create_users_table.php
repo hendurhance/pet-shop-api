ee0159ed4a96a47a5be3b395e4b33352f5c22fa7
Endurance, the Martian <afriviral@gmail.com>
2023-04-11 10:55:00 +0100

    setup/ users, jwt, and files table

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique()->index();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_admin')->default(false);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            // $table->string('avatar')->nullable();
+            // Avatar UUID of the image stored into the files table 
+            $table->uuid('avatar')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->boolean('is_marketing')->default(false);
+            $table->dateTime('last_login_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
