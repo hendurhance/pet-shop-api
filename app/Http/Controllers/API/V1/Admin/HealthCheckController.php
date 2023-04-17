343c322ca1e8a42d36f87d97752e7f56bf363e3d
Endurance, the Martian <afriviral@gmail.com>
2023-04-17 15:44:20 +0100

    added/ package-lock.json

--- app/Http/Controllers/API/V1/Admin/HealthCheckController.php
+++ app/Http/Controllers/API/V1/Admin/HealthCheckController.php
@@ -11,6 +11,25 @@ class HealthCheckController extends Controller
      * Get health check
      * @return \App\Traits\HttpResponse
      */
+    /**
+     * @OA\Get(
+     *    path="/api/v1/admin/health-check",
+     *    tags={"Health Check"},
+     *   summary="Get health check",
+     *  description="Get health check",
+     * @OA\Response(
+     *       response=200,
+     *      description="Success",
+     *    @OA\JsonContent(
+     *         @OA\Property(
+     *            property="status",
+     *          type="string",
+     *        example="ok"
+     *     ),
+     *   ),
+     * ),
+     * )
+     */
     public function index()
     {
         return $this->success(['status' => 'ok'], 'Health check for admin api is ok');
---
+++ app/Http/Controllers/API/V1/SwaggerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SwaggerController extends Controller
+{
+    /**
+     * Get swagger
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        return view('swagger.index');
+    }
+    
+}
